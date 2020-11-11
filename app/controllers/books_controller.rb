a3c450400e89af2bf9b33aaaf934d47ae16a79ed
ashu02-git <shuhei.abe1107@gmail.com>
2020-11-11 08:55:28 +0000

    [Add] 編集画面のエラーメッセージ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,19 +19,22 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to show_book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to show_book_path(@book.id)
+    else
+      render action: :edit
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
