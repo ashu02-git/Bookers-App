3c77b7211fedc531429be752255ae7000a46a35f
ashu02-git <shuhei.abe1107@gmail.com>
2020-11-12 03:01:43 +0000

    [Add] ビュー画面のレイアウト編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -12,8 +13,12 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to show_book_path(book.id)
+    if book.save
+      redirect_to show_book_path(book.id)
+    else
+      @books = Book.all
+      render action: :index
+    end
   end
 
   def edit
